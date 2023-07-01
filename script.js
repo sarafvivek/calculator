d309e4db133beabecf224a3d6b02f3b143d469b0
Vivek Saraf <137922905+sarafvivek@users.noreply.github.com>
2023-07-02 00:16:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var expectedMileage = 0;
+
 function calculateMileage() {
   var distance = parseFloat(document.getElementById("distance").value);
   var totalAmount = parseFloat(document.getElementById("totalAmount").value);
@@ -13,32 +15,7 @@ function calculateMileage() {
   var liters = totalAmount / todayPrice;
   var fuelConsumption = distance / liters;
 
-  var expectedMileage;
-  switch (vehicle) {
-    case "truck1":
-      expectedMileage = 7;
-      break;
-    case "truck2":
-      expectedMileage = 3.5;
-      break;
-    case "truck3":
-      expectedMileage = 5.5;
-      break;
-    case "truck4":
-      expectedMileage = 6;
-      break;
-    case "truck5":
-      expectedMileage = 4;
-      break;
-    case "truck6":
-      expectedMileage = 4;
-      break;
-    case "truck7":
-      expectedMileage = 7;
-      break;
-    default:
-      expectedMileage = 0;
-  }
+  checkTruck(vehicle);
 
   var mileageDifference = fuelConsumption - expectedMileage;
 
@@ -68,4 +45,176 @@ function calculateMileage() {
 
   document.getElementById("rHead").innerHTML = resultHead;
   document.getElementById("rMessage").innerHTML = resultMessage;
+}
+
+function checkTruck(vehicle) {
+  switch (vehicle) {
+    case "BRO1GA9117":
+      expectedMileage = 5.5;
+      break;
+    case "BRO1GA9217":
+      expectedMileage = 5.5;
+      break;
+    case "BR01GA9517":
+      expectedMileage = 5.5;
+      break;
+    case "BR01GA9617":
+      expectedMileage = 5.5;
+      break;
+    case "BRO1GA9717":
+      expectedMileage = 5.5;
+      break;
+    case "WB29A9222":
+      expectedMileage = 5.5;
+      break;
+    case "WB23D4630":
+      expectedMileage = 5.5;
+      break;
+    case "NL02Q3966":
+      expectedMileage = 5.5;
+      break;
+    case "MP20HB4617":
+      expectedMileage = 4;
+      break;
+    case "MP20HB5217":
+      expectedMileage = 4;
+      break;
+    case "WB23C8182":
+      expectedMileage = 5.5;
+      break;
+    case "JH01AY1617":
+      expectedMileage = 5.5;
+      break;
+    case "JH01AY2917":
+      expectedMileage = 5.5;
+      break;
+    case "WB51A7505":
+      expectedMileage = 5.5;
+      break;
+    case "JH01AY1217":
+      expectedMileage = 5.5;
+      break;
+    case "WB41C6377":
+      expectedMileage = 7;
+      break;
+    case "UP93AT1285":
+      expectedMileage = 4;
+      break;
+    case "UP93AT1287":
+      expectedMileage = 4;
+      break;
+    case "UP93AT1288":
+      expectedMileage = 4;
+      break;
+    case "UP93AT1289":
+      expectedMileage = 4;
+      break;
+    case "WB25B1517":
+      expectedMileage = 5.5;
+      break;
+    case "WB23B9771":
+      expectedMileage = 3.5;
+      break;
+    case "WB15C5160":
+      expectedMileage = 5.5;
+      break;
+    case "BR01GG4262":
+      expectedMileage = 6;
+      break;
+    case "BR01GL2314":
+      expectedMileage = 7;
+      break;
+    case "MP49H0717":
+      expectedMileage = 4;
+      break;
+    case "MP49H0917":
+      expectedMileage = 4;
+      break;
+    case "MP49H1417":
+      expectedMileage = 4;
+      break;
+    case "UP78CN6293":
+      expectedMileage = 4;
+      break;
+    case "UP78CN6294":
+      expectedMileage = 4;
+      break;
+    case "UP78CN6295":
+      expectedMileage = 4;
+      break;
+    case "UP78CN6291":
+      expectedMileage = 4;
+      break;
+    case "UP93AT1481":
+      expectedMileage = 4;
+      break;
+    case "MP49H1517":
+      expectedMileage = 4;
+      break;
+    case "UP78CN6287":
+      expectedMileage = 4;
+      break;
+    case "UP78CN6288":
+      expectedMileage = 4;
+      break;
+    case "UP78CN6289":
+      expectedMileage = 4;
+      break;
+    case "UP78CN6290":
+      expectedMileage = 4;
+      break;
+    case "BR01GL1068":
+      expectedMileage = 4;
+      break;
+    case "UP93AT1483":
+      expectedMileage = 4;
+      break;
+    case "BR01GL1069":
+      expectedMileage = 4;
+      break;
+    case "NL01L0655":
+      expectedMileage = 4;
+      break;
+    case "WB25J4273":
+      expectedMileage = 5.5;
+      break;
+    case "WB25J6229":
+      expectedMileage = 5.5;
+      break;
+    case "BR01GL6978":
+      expectedMileage = 7;
+      break;
+    case "BR01GL6979":
+      expectedMileage = 7;
+      break;
+    case "BR01GL0816":
+      expectedMileage = 5.5;
+      break;
+    case "WB37D1259":
+      expectedMileage = 4;
+      break;
+    case "WB37D1261":
+      expectedMileage = 4;
+      break;
+    case "WB37D1262":
+      expectedMileage = 4;
+      break;
+    case "WB37D1306":
+      expectedMileage = 4;
+      break;
+    case "WB15D4978":
+      expectedMileage = 5.5;
+      break;
+    case "BR01GL8087":
+      expectedMileage = 7;
+      break;
+    case "JH01AY4907":
+      expectedMileage = 5.5;
+      break;
+    case "JH02W9015":
+      expectedMileage = 5.5;
+      break;
+    default:
+      expectedMileage = 0;
+  }
 }
